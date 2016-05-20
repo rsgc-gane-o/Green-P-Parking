bf5dcbd56a107ada971198b417a7b48968c559a4
Student <student@macbook-pro-gm-9.local>
2016-05-20 10:18:46 -0400

    Initial Commit

---
+++ Green P ParkingTests/Green_P_ParkingTests.swift
@@ -0,0 +1,36 @@
+//
+//  Green_P_ParkingTests.swift
+//  Green P ParkingTests
+//
+//  Created by Student on 2016-05-20.
+//  Copyright © 2016 Oliver Gane. All rights reserved.
+//
+
+import XCTest
+@testable import Green_P_Parking
+
+class Green_P_ParkingTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
